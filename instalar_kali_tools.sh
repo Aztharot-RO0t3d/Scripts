211c5de56cf43366e2fd202c507082e3ceecdccc
Aztharot <130052303+Aztharot-RO0t3d@users.noreply.github.com>
2024-09-13 00:43:56 -0600

    MacOs_kali_tools_installer.sh

--- instalar_kali_tools.sh
+++ instalar_kali_tools.sh
@@ -1,38 +1,30 @@
 #!/bin/bash
 
-# Script para instalar Homebrew y herramientas de Kali Linux en macOS
-
-# Función para imprimir mensajes con formato
 print_msg() {
     echo -e "\n\e[1;32m$1\e[0m\n"
 }
 
-# Instalación de Homebrew si no está instalado
 if ! command -v brew &> /dev/null; then
     print_msg "Instalando Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     
-    # Configuración de PATH para Homebrew
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
 else
     print_msg "Homebrew ya está instalado."
 fi
 
-# Actualización y limpieza de Homebrew
 print_msg "Actualizando y limpiando Homebrew..."
 brew update
 brew upgrade
 brew cleanup
 
-# Agregar repositorios necesarios
 print_msg "Agregando repositorios para herramientas de seguridad..."
 brew tap homebrew/cask
 brew tap homebrew/core
 brew tap caffix/amass
 brew tap go-task/task
 
-# Lista de herramientas de Kali Linux para instalar
 tools=(
     "nmap"
     "wireshark"
@@ -46,7 +38,6 @@ tools=(
     "task"
 )
 
-# Instalación de herramientas
 print_msg "Instalando herramientas de Kali Linux compatibles con macOS..."
 for tool in "${tools[@]}"; do
     if brew list -1 | grep -q "^${tool}\$"; then
@@ -57,7 +48,6 @@ for tool in "${tools[@]}"; do
     fi
 done
 
-# Buscar herramientas adicionales de Kali Linux compatibles con macOS
 print_msg "Buscando herramientas adicionales de Kali Linux compatibles..."
 search_results=$(brew search kali)
 
